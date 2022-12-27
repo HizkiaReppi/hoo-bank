1596eb15227f2a3410f30ca375478de7dced5ff3
Hizkia Jefren Reppi <hizkiajefren0501@gmail.com>
2022-12-27 19:08:41 +0800

    Features: Hero Section & Stats Section

--- frontend/src/components/GetStarted.jsx
+++ frontend/src/components/GetStarted.jsx
@@ -1,7 +1,21 @@
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+import styles from "../style"
+import { arrowUp } from "../assets"
+
+const GetStarted = () => (
+  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-0.5 cursor-pointer`}>
+    <div className={`${styles.flexCenter} flex-col bg-primary w-full h-full rounded-full`}>
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-lg leading-6 mr-2">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img src={arrowUp} alt="Arrow Up" className="w-6 h-6 object-contain" />
+      </div>
+      <p className="font-poppins font-medium text-lg leading-6">
+        <span className="text-gradient">Started</span>
+      </p>
+    </div>
+  </div>
+)
+
 
 export default GetStarted
--- frontend/src/components/Stats.jsx
+++ frontend/src/components/Stats.jsx
@@ -1,7 +1,15 @@
-const Stats = () => {
-  return (
-    <div>Stats</div>
-  )
-}
+import { stats } from '../constants'
+import styles from '../style'
+
+const Stats = () => (
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 md-6`}>
+    {stats.map((stat) => (
+      <div key={stat.id} className={`flex-1 flex justify-start items-center m-3`}>
+        <h4 className='font-poppins font-semibold xs:text-[40px] text-3xl xs:leading-[54px] leading-[43px] text-white'>{stat.value}</h4>
+        <p className='font-poppins xs:text-xl text-base xs:leading-[26px] leading-5 text-gradient uppercase ml-3'>{stat.title}</p>
+      </div>
+    ))}
+  </section>
+)
 
 export default Stats
