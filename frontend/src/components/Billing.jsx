fa0243cf933246c7385ecba35a50d167e117a14f
Hizkia Jefren Reppi <hizkiajefren0501@gmail.com>
2022-12-28 13:39:40 +0800

    Features: Billing Section & CardDeal Section

--- frontend/src/components/Billing.jsx
+++ frontend/src/components/Billing.jsx
@@ -1,7 +1,25 @@
-const Billing = () => {
-  return (
-    <div>Billing</div>
-  )
-}
+import { apple, bill, google } from "../assets"
+import styles, { layout } from "../style"
+
+const Billing = () => (
+  <section id="product" className={`${layout.sectionReverse}`}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="Billing" className="w-full h-full relative z-[5]" />
+      <div className="absolute z-[3] -left-1/2 top-0 w-1/2 h-1/2 rounded-full white__gradient" />
+      <div className="absolute z-0 -left-1/2 bottom-0 w-1/2 h-1/2 rounded-full pink__gradient" />
+    </div>
+    <div className={`${layout.sectionInfo}`}>
+      <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing & invoicing</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Elit enim sad massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.
+      </p>
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="App Store" className="w-32 h-[42px] object-contain cursor-pointer mr-5" />
+        <img src={google} alt="Google Play" className="w-32 h-[42px] object-contain cursor-pointer" />
+      </div>
+    </div>
+  </section>
+)
+
 
 export default Billing
--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none rounded-md ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-lg text-primary outline-none rounded-xl ${styles}`}>
       Get Started
     </button>
   )
