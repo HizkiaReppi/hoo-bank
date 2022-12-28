a2c32e387447e4d8251a957870c748b9473cb79a
Hizkia Jefren Reppi <hizkiajefren0501@gmail.com>
2022-12-28 14:12:39 +0800

    Features: Add Testimonials, Clients & CTA Section

--- frontend/src/components/Clients.jsx
+++ frontend/src/components/Clients.jsx
@@ -1,7 +1,16 @@
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+import { clients } from "../constants"
+import styles from "../style"
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+          <img src={client.logo} alt="Logo Client" className="sm:w-48 w-[100px] object-contain" />
+        </div>
+      ))}
+    </div>
+  </section>
+)
 
 export default Clients
--- frontend/src/components/FeedbackCard.jsx
+++ frontend/src/components/FeedbackCard.jsx
@@ -1,6 +1,19 @@
-const FeedbackCard = () => {
+import { quotes } from "../assets"
+
+const FeedbackCard = ({ content, name, title, img }) => {
   return (
-    <div>FeedbackCard</div>
+    <div className="flex justify-between flex-col px-10 py-12 rounded-3xl max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img src={quotes} alt="Double Qoutes" className="w-11 h-7 object-contain" />
+      <p className="font-poppins text-lg leading-8 text-white my-10">{content}</p>
+
+      <div className="flex">
+        <img src={img} alt={name} className="w-12 h-12 rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semibold text-xl leading-8 text-white">{name}</h4>
+          <p className="font-poppins text-base leading-6 text-dimWhite">{title}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
